73ab615e3b977e13b1fa36cdb613d07f66bc2c4a
Tim Raymond <xtjraymondx@gmail.com>
2014-08-21 07:51:15 -0400

    Remove Keywords, reorganization
    We can probably do without keywords to keep a bit of flexibility in
    Braai. The intention is that the first command encountered will be the
    name of the retriever that we're going to invoke, the path being the
    following dots and commands that occur after it. This path can be some
    signal that the retriever looks at to know which thing it should
    retrieve.
    
    Also roughly reorganized the lexer to have public API first, helper
    functions, then stateFns so it's a bit easier to navigate

--- lex/lexer.go
+++ lex/lexer.go
@@ -1,13 +1,19 @@
 package lex
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
 
 type lexer struct {
 	input string    // the string being scanned
 	items chan item // holds scanned items
 	state stateFn   // current state of the lexer
-	start int       // start position in the input
-	pos   int       // current position in the input
+	start int       // start position in the input of the next token
+	pos   int       // current position in the input, will mark the end of the next token
+	width int       // width of the last read rune
 }
 
 // Represents different types of lexed items.
@@ -15,27 +21,31 @@ type itemType int
 
 type stateFn func(*lexer) stateFn
 
+const letters = "abcdefghijklmnopqrstuvwxyz"
+const hex = "0123456789abcdef"
+
+const eof = -1
+
 const (
 	itemText itemType = iota
 	itemLeftMeta
 	itemRightMeta
-	itemProductName
-	itemAttachment
+	itemCommand
+	itemDot
+	itemSlash
 	itemArgumentOpen
 	itemArgumentClose
 	itemIdentifier
 	itemEOF
+	itemSpace
+	itemError
 )
-
-var keywords = map[string]itemType{
-	"product.name": itemProductName,
-	"attachments":  itemAttachment,
-}
 
 // Represents a scanned item. Includes the string value encompassing the item.
 // Also known as a Lexeme or Token in other lexer literature
 type item struct {
 	Type  itemType
+	Pos   int
 	Value string
 }
 
@@ -50,92 +60,151 @@ func (self *lexer) NextToken() item {
 	}
 }
 
-// emits a new item into the lexer's items channel
-func (self *lexer) emit(t itemType) {
-	self.items <- item{t, self.input[self.start:self.pos]}
-	self.start = self.pos
-}
-
 // Provides a new lexer for the given document
 func NewLexer(document string) *lexer {
 	return &lexer{input: document, state: lexText, items: make(chan item, 2)}
 }
 
+// emits a new item into the lexer's items channel
+func (self *lexer) emit(t itemType) {
+	self.items <- item{t, self.start, self.input[self.start:self.pos]}
+	self.start = self.pos
+}
+
+func (self *lexer) backup() {
+	self.pos -= self.width
+}
+
+func (self *lexer) next() rune {
+	if int(self.pos) >= len(self.input) {
+		return eof
+	}
+	r, w := utf8.DecodeRuneInString(self.input[self.pos:])
+	self.width = w
+	self.pos += w
+	return r
+}
+
+func (self *lexer) accept(valid string) bool {
+	if strings.IndexRune(valid, self.next()) >= 0 {
+		return true
+	}
+	self.backup()
+	return false
+}
+
+func (self *lexer) acceptRun(valid string) {
+	for strings.IndexRune(valid, self.next()) >= 0 {
+	}
+	self.backup()
+}
+
+func (l *lexer) peek() rune {
+	r := l.next()
+	l.backup()
+	return r
+}
+
+func (l *lexer) errorf(format string, args ...interface{}) stateFn {
+	l.items <- item{itemError, l.start, fmt.Sprintf(format, args...)}
+	return nil
+}
+
 func lexText(l *lexer) stateFn {
 	for {
-		if l.pos == len(l.input) {
-			l.emit(itemText)
-			return lexEOF
-		}
 		if strings.HasPrefix(l.input[l.pos:], "{{") {
 			l.emit(itemText)
 			return lexLeftMeta
-		} else {
-			l.pos += 1
+		}
+		if l.next() == eof {
+			break
 		}
 	}
-	l.emit(itemText)
-	return lexEOF
+	if l.pos > l.start {
+		l.emit(itemText)
+	}
+	l.emit(itemEOF)
+	return nil
 }
 
 func lexLeftMeta(l *lexer) stateFn {
-	l.pos += 2
+	l.pos += len("{{")
 	l.emit(itemLeftMeta)
 	return lexInsideAction
 }
 
 func lexRightMeta(l *lexer) stateFn {
-	l.pos += 2
+	l.pos += len("}")
 	l.emit(itemRightMeta)
 	return lexText
 }
 
+func lexSpace(l *lexer) stateFn {
+	for isSpace(l.peek()) {
+		l.next()
+	}
+	l.emit(itemSpace)
+	return lexInsideAction
+}
+
 func lexInsideAction(l *lexer) stateFn {
-	for {
-		if l.pos == len(l.input) {
-			l.emit(itemText)
-			return lexEOF
-		}
-		if strings.HasPrefix(l.input[l.pos:], "}}") {
-			l.emit(keywords[l.input[l.start:l.pos]])
-			return lexRightMeta
-		} else if string(l.input[l.pos]) == "(" {
-			l.emit(keywords[l.input[l.start:l.pos]])
-			return lexArgumentOpen
-		} else {
-			l.pos += 1
-		}
+	switch r := l.next(); {
+	case unicode.IsLetter(r):
+		return lexCommand
+	case isSpace(r):
+		return lexSpace
+	case r == eof:
+		return l.errorf("Unexpected end of action")
+	case r == '}':
+		return lexRightMeta
+	case r == '.':
+		l.emit(itemDot)
+		return lexInsideAction
+	case r == '(':
+		return lexArgumentOpen
+	case r == '/':
+		return lexCloser
+	default:
+		return l.errorf("Unexpected character %#U", r)
 	}
 }
 
+func lexCloser(l *lexer) stateFn {
+	l.emit(itemSlash)
+	return lexCommand
+}
+
+func lexCommand(l *lexer) stateFn {
+	l.acceptRun(letters)
+	l.emit(itemCommand)
+	return lexInsideAction
+}
+
 func lexArgumentOpen(l *lexer) stateFn {
-	l.pos += 1
 	l.emit(itemArgumentOpen)
 	return lexArgument
 }
 
 func lexArgumentClose(l *lexer) stateFn {
-	l.pos += 1
-	l.emit(itemArgumentClose)
-	return lexRightMeta
+	r := l.next()
+	if r == ')' {
+		l.emit(itemArgumentClose)
+	} else {
+		return l.errorf("Unexpected character in argument: %#U", r)
+	}
+	return lexInsideAction
 }
 
 func lexArgument(l *lexer) stateFn {
-	for {
-		if l.pos == len(l.input) {
-			l.emit(itemText)
-			return lexEOF
-		}
-		if strings.HasPrefix(l.input[l.pos:], ")") {
-			l.emit(itemIdentifier)
-			return lexArgumentClose
-		} else {
-			l.pos += 1
-		}
-	}
+	l.acceptRun(hex)
+	l.emit(itemIdentifier)
+	return lexArgumentClose
 }
 
-func lexEOF(l *lexer) stateFn {
-	l.emit(itemEOF)
-	return nil
+func isSpace(input rune) bool {
+	if input == ' ' || input == '\t' {
+		return true
+	} else {
+		return false
+	}
 }
--- lex/lexer_test.go
+++ lex/lexer_test.go
@@ -12,42 +12,71 @@ type lexTest struct {
 
 var lexTests = []lexTest{
 	{"plain", "This is some plain old *markdown*", []item{
-		{itemText, "This is some plain old *markdown*"},
+		{itemText, 0, "This is some plain old *markdown*"},
 	}},
 	{"other markdown", "This is _other_ markdown", []item{
-		{itemText, "This is _other_ markdown"},
+		{itemText, 0, "This is _other_ markdown"},
 	}},
 	{"markdown with newlines", "Markdown with \n\n Newlines...", []item{
-		{itemText, "Markdown with \n\n Newlines..."},
+		{itemText, 0, "Markdown with \n\n Newlines..."},
 	}},
 	{"markdown with opening braai tag", "The {{", []item{
-		{itemText, "The "},
-		{itemLeftMeta, "{{"},
+		{itemText, 0, "The "},
+		{itemLeftMeta, 0, "{{"},
+		{itemError, 0, "Unexpected end of action"},
 	}},
 	{"markdown with {{product.name}} braai tag", "The {{product.name}}", []item{
-		{itemText, "The "},
-		{itemLeftMeta, "{{"},
-		{itemProductName, "product.name"},
-		{itemRightMeta, "}}"},
+		{itemText, 0, "The "},
+		{itemLeftMeta, 0, "{{"},
+		{itemCommand, 0, "product"},
+		{itemDot, 0, "."},
+		{itemCommand, 0, "name"},
+		{itemRightMeta, 0, "}}"},
 	}},
 	{"markdown with multiple {{product.name}} braai tags", "The {{product.name}} is called the {{product.name}}", []item{
-		{itemText, "The "},
-		{itemLeftMeta, "{{"},
-		{itemProductName, "product.name"},
-		{itemRightMeta, "}}"},
-		{itemText, " is called the "},
-		{itemLeftMeta, "{{"},
-		{itemProductName, "product.name"},
-		{itemRightMeta, "}}"},
+		{itemText, 0, "The "},
+		{itemLeftMeta, 0, "{{"},
+		{itemCommand, 0, "product"},
+		{itemDot, 0, "."},
+		{itemCommand, 0, "name"},
+		{itemRightMeta, 0, "}}"},
+		{itemText, 0, " is called the "},
+		{itemLeftMeta, 0, "{{"},
+		{itemCommand, 0, "product"},
+		{itemDot, 0, "."},
+		{itemCommand, 0, "name"},
+		{itemRightMeta, 0, "}}"},
 	}},
-	{"attachment with arguments", "Here's an awesome attachment {{attachments(350661)}}", []item{
-		{itemText, "Here's an awesome attachment "},
-		{itemLeftMeta, "{{"},
-		{itemAttachment, "attachments"},
-		{itemArgumentOpen, "("},
-		{itemIdentifier, "350661"},
-		{itemArgumentClose, ")"},
-		{itemRightMeta, "}}"},
+	{"attachment with arguments", "Here's an awesome attachment {{ attachments(350661) }}", []item{
+		{itemText, 0, "Here's an awesome attachment "},
+		{itemLeftMeta, 0, "{{"},
+		{itemSpace, 0, " "},
+		{itemCommand, 0, "attachments"},
+		{itemArgumentOpen, 0, "("},
+		{itemIdentifier, 0, "350661"},
+		{itemArgumentClose, 0, ")"},
+		{itemSpace, 0, " "},
+		{itemRightMeta, 0, "}}"},
+	}},
+	{"attachment in a callout", "# Editors’ Choice Awards{{callout}}{{ attachments(349807) }}{{/callout}}", []item{
+		{itemText, 0, "# Editors’ Choice Awards"},
+		{itemLeftMeta, 0, "{{"},
+		{itemCommand, 0, "callout"},
+		{itemRightMeta, 0, "}}"},
+		{itemText, 0, ""},
+		{itemLeftMeta, 0, "{{"},
+		{itemSpace, 0, " "},
+		{itemCommand, 0, "attachments"},
+		{itemArgumentOpen, 0, "("},
+		{itemIdentifier, 0, "349807"},
+		{itemArgumentClose, 0, ")"},
+		{itemSpace, 0, " "},
+		{itemRightMeta, 0, "}}"},
+		{itemText, 0, ""}, // These are transition points between braai/text
+		{itemLeftMeta, 0, "{{"},
+		{itemSlash, 0, "/"},
+		{itemCommand, 0, "callout"},
+		{itemRightMeta, 0, "}}"},
 	}},
 }
 
@@ -57,7 +86,7 @@ func collect(t *lexTest) (items []item) {
 	for {
 		item := lexer.NextToken()
 		items = append(items, item)
-		if item.Type == itemEOF {
+		if item.Type == itemEOF || item.Type == itemError {
 			break
 		}
 	}
